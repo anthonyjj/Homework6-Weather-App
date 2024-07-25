167e160f3cd034fc8c49c7a167149feb975d4ec2
anthonyjj <156860966+anthonyjj@users.noreply.github.com>
2024-07-25 00:05:12 -0700

    made a fetch call

--- script.js
+++ script.js
@@ -8,6 +8,9 @@ const currentTempEl = document.getElementById('current-temp');
 
 const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday,']
 const months = ['Jan', 'Feb', 'Mar', 'Apr', 'Mar', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+const API_KEY = 'd28ffb69eefcec793fa2f4e40c3a688c';
+
 setInterval(() => {
     const time = new Date();
     const month = time.getMonth();
@@ -24,3 +27,37 @@ setInterval(() => {
     dateEl.innerHTML = days[day] + ', ' + date+ ' ' + months[month]
     
 },1000);
+
+getWeatherData()
+function getWeatherData () {
+    navigator.geolocation.getCurrentPosition((success) => {
+        console.log(success);
+
+        let { latitude, longitude } = success.coords;
+
+        fetch('api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${API_key}').then(res => res.json()).then(data=>{
+            console.log(data)
+            showWeatherData(data);
+        })
+    })
+}
+
+function showWeatherData (data){
+    let {humidity, pressure, sunrise, sunset, wind_speed} = data.current;
+
+    currentWeatherItemsEl.innerHTML =
+                 `<div class="weather-item">
+                        <div>Humidity</div>
+                        <div>${humidity}%</div>
+                    </div>
+                    <div class="weather-item">
+                        <div>Wind Speed</div>
+                        <div>${wind_speed}</div>
+                    </div>
+                    <div class="weather-item">
+                        <div>Chance Of Rain</div>
+                        <div>95.2%</div>
+                    </div>`;
+
+    
+}
